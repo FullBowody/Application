beb267e0a5c0fe6e52d5d7f8c106fd645595157f
FurWaz <fur.waz06@gmail.com>
2022-10-13 18:44:08 +0200

    Improved 3D preview

--- src/views/Preview.vue
+++ src/views/Preview.vue
@@ -13,11 +13,22 @@ let scene;
 let camera;
 /**@type {THREE.Renderer} */
 let renderer;
-let rotation = 0;
+
+const MOUSE_SENSITIVITY = 1;
+const mouse = { x: 0, y: 0, state: "up" };
+let camDist = 4;
+let rotation = {x: 0, y: Math.PI/4};
+let lastTime = 0;
 
 function loop(time) {
-    rotation = time / 4000;
-    camera.position.set(Math.sin(rotation) * 4, 3, Math.cos(rotation) * 4);
+    const dt = time - lastTime;
+    lastTime = time;
+    if (mouse.state == "up") rotation.x += dt / 2000;
+    camera.position.set(
+        Math.sin(rotation.x) * Math.sin(rotation.y) * camDist,
+        Math.cos(rotation.y) * camDist,
+        Math.cos(rotation.x) * Math.sin(rotation.y) * camDist
+    );
     camera.lookAt(0, 0, 0);
     renderer.render(scene, camera);
 }
@@ -29,13 +40,37 @@ function init() {
     canvas.width = rect.width;
     canvas.height = rect.height;
 
-    window.addEventListener('resize', () => {
+    canvas.addEventListener('resize', () => {
         rect = container.getBoundingClientRect();
         canvas.width = rect.width;
         canvas.height = rect.height;
         camera.aspect = canvas.width / canvas.height;
         camera.updateProjectionMatrix();
         renderer.setSize(canvas.width, canvas.height);
+    });
+
+    canvas.addEventListener("mousedown", ev => {
+        mouse.state = "down";
+        mouse.x = ev.clientX;
+        mouse.y = ev.clientY;
+    });
+    canvas.addEventListener("mouseup", ev => {
+        mouse.state = "up";
+        mouse.x = ev.clientX;
+        mouse.y = ev.clientY;
+    });
+    canvas.addEventListener("mousemove", ev => {
+        if (mouse.state == "down") {
+            let deltaX = ev.clientX - mouse.x;
+            let deltaY = ev.clientY - mouse.y;
+            mouse.x = ev.clientX;
+            mouse.y = ev.clientY;
+            rotation.x -= deltaX * MOUSE_SENSITIVITY * 0.01;
+            rotation.y = Math.min(Math.max(rotation.y - deltaY * MOUSE_SENSITIVITY * 0.01, 0.1), Math.PI - 0.1);
+        }
+    });
+    canvas.addEventListener("wheel", ev => {
+        camDist = Math.min(Math.max(camDist + ev.deltaY * 0.01, 1), 10);
     });
 
     renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
