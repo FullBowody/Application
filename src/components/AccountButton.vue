2a6176a40704e4fcf468252639803a7fcd57d292
FurWaz <fur.waz06@gmail.com>
2022-10-13 18:43:50 +0200

    Set default user icon if not image

--- src/components/AccountButton.vue
+++ src/components/AccountButton.vue
@@ -13,7 +13,12 @@
                 </svg>
             </div>
             <div v-if="User.CurrentUser != null">
-                <img :src="User.CurrentUser.icon" alt="user icon" class="h-8 rounded-lg border border-2 transition-all" :class="this.active? 'border-blue-500':'border-slate-600'">
+                <img v-if="User.CurrentUser.icon != ''" :src="User.CurrentUser.icon" alt="user icon" class="h-8 rounded-lg border border-2 transition-all" :class="this.active? 'border-blue-500':'border-slate-600'">
+                <div v-if="User.CurrentUser.icon == ''">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                </div>
             </div>
         </div>
     </div>
