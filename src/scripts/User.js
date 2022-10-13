de842a9b172cbbb3bed80716c15c55a9b0801cb6
FurWaz <fur.waz06@gmail.com>
2022-10-13 18:44:47 +0200

    Improved user object

--- src/scripts/User.js
+++ src/scripts/User.js
@@ -1,4 +1,13 @@
+import API from "./API";
+
 class User {
+    static ROLE = {
+        UNKNOWN: 0,
+        ADMIN: 1,
+        USER: 2,
+        GUEST: 3
+    }
+
     static #currentUser = null;
     static get CurrentUser() {
         return User.#currentUser || User.fromLocalStorage();
@@ -20,6 +29,8 @@ class User {
     email = "";
     icon = "";
     token = "";
+    id = 0;
+    role = User.ROLE.UNKNOWN;
 
     constructor(infos) {
         this.username = infos.username??this.username;
@@ -27,7 +38,90 @@ class User {
         this.email = infos.email??this.email;
         this.token = infos.token??this.token;
         this.icon = infos.icon??this.icon;
+        this.id = infos.id??this.id;
+        this.role = infos.role??this.role;
         User.#currentUser = this;
+    }
+
+    getCredentials() {
+        if (this.token.trim() != "")
+            return API.Credentials.fromToken(this.token);
+        return API.Credentials.fromUsernamePassword(this.username, this.password);
+    }
+
+    makeRequest(path, method=undefined, body=undefined, type=undefined, headers=undefined) {
+        return new Promise((resolve, reject) => {
+            const credentials = this.getCredentials();
+            if (!credentials.isValid()) {
+                reject("Invalid user credentials");
+                return;
+            }
+
+            const req = (creds) => API.execute_logged(path, method, creds, body, type, headers);
+            req(credentials).then(data => {
+                resolve(data);
+            }).catch(err => {
+                if (err.status == 401) {
+                    this.fetchToken().then((token) => {
+                        req(API.Credentials.fromToken(token)).then(data => {
+                            resolve(data);
+                        }).catch(err => {
+                            reject(err);
+                        });
+                    }).catch(err => {
+                        reject(err);
+                    });
+                }
+            });
+        });
+    }
+
+    fetchToken() {
+        return new Promise((resolve, reject) => {
+            if (this.username.trim() == "" || this.password.trim() == "") {
+                reject("Invalid user credentials");
+                return;
+            }
+
+            API.execute(API.ROUTE.LOGIN, API.METHOD.POST, {username: this.username, password: this.password}).then(token => {
+                this.token = token;
+                this.save();
+                resolve(token);
+            }).finally(() => {
+                
+            }).catch(err => {
+                switch (err.status) {
+                    case 404:
+                        reject("Invalid username");
+                        break;
+                    case 403:
+                        reject("Invalid password");
+                        break;
+                    default:
+                        reject("A problem occured");
+                        break;
+                }
+            });
+        });
+    }
+
+    fetchInformations() {
+        return new Promise((resolve, reject) => {
+            const credentials = this.getCredentials();
+            if (!credentials.isValid()) {
+                reject("Invalid user credentials");
+                return;
+            }
+
+            this.makeRequest(API.ROUTE.ME).then(data => {
+                this.username = data.username;
+                this.email = data.email;
+                this.save();
+                resolve(data);
+            }).catch(err => {
+                reject(err);
+            });
+        })
     }
 
     save() {
