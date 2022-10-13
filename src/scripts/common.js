f81f76c26aa065717a2af1e21535e0410f20531f
FurWaz <fur.waz06@gmail.com>
2022-10-13 18:47:32 +0200

    Update common.js

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -1,12 +1,36 @@
+const STATE_ERROR = 0;
+const STATE_STOPPED = 1;
+const STATE_STARTED = 2;
+const STATE_STARTING = 3;
+const STATE_STOPPING = 4;
+
+const STATE = {
+    STATE_ERROR,
+    STATE_STOPPED,
+    STATE_STARTED,
+    STATE_STARTING,
+    STATE_STOPPING,
+    stateString
+};
+
+function stateString(state) {
+    switch (state) {
+        case STATE_ERROR: return "Error";
+        case STATE_STOPPED: return "Stopped";
+        case STATE_STARTED: return "Started";
+        case STATE_STARTING: return "Starting";
+        case STATE_STOPPING: return "Stopping";
+        default: return "Unknown";
+    }
+}
+
 function openLink(link) {
     if (link.startsWith("http"))
         ipc.send("openLink", link);
     else window.location.href = link;
 }
 
-function send(channel, data) {
-    return ipc.send(channel, data);
+export {
+    openLink,
+    STATE
 }
-
-window.send = send;
-export { openLink };
